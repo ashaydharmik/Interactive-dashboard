2a2f4301e5a6be791971458962d99e4709b9e7e2
ashaydharmik <ashaydharmik807@gmail.com>
2024-03-06 21:38:41 +0530

    added hover effect

--- src/components/Sidebar/Folders/Products.js
+++ src/components/Sidebar/Folders/Products.js
@@ -24,7 +24,7 @@ const Products = () => {
           </div>
           <div
             className="folder-box"
-            style={{ background: " rgb(230, 230, 230)" }}
+          
           >
             <p>
               <span>
